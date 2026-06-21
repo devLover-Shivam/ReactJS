aaadba067541133ddea08359516df6be72113b63
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-06-21 14:09:51 +0530

    Restructure notes and add first React project

--- notes/01-import-export/importExport/app.js
+++
@@ -1,7 +0,0 @@
-// export are of two types -named -default(can only be one)
-const user = 'Shivam';
-export const age = 22;// named export
-export const skills = ['Frontend','Backend'];// named export
-export default user;// default export
-
-
