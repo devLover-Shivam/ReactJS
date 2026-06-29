0da3a56ac308181f1057e959197338f930625c6d
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-06-29 18:18:21 +0530

    Add Navbar of section1 of ui project

--- projects/08-ui-project/src/App.jsx
+++ projects/08-ui-project/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <div>
       <Section1/>
       <Section2/>
+      
     </div>
   )
 }
---
+++ projects/08-ui-project/src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-15 py-5' >
+      <h4 className='bg-black text-white uppercase px-6 py-2 rounded-full text-s'>Target Audience</h4>
+      <button className='bg-gray-300 text-black uppercase px-6 py-2 rounded-full tracking-widest text-s'>Digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
